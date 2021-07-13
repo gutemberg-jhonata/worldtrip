d4055f3da7b4510ba5704d7543975f0b07733750
Jhonata <gutemberg.jhonata@gmail.com>
2021-07-12 21:58:34 -0300

    Add colors to theme

--- styles/theme.ts
+++ styles/theme.ts
@@ -5,5 +5,16 @@ export const theme = extendTheme({
     body: "Poppins, sans-serif",
     heading: "Poppins, sans-serif",
     barlow: "Barlow, sans-serif "
+  },
+  colors: {
+    gray: {
+      100: "#F5F8FA",
+      200: "#DADADA",
+      400: "#999999",
+      600: "#47585B"
+    },
+    yellow: {
+      500: "#FFBA08"
+    }
   }
 })
