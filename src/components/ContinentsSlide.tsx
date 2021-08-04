05c6e8be982e0d0aceec6a9b593192ce147013d6
Jhonata <gutemberg.jhonata@gmail.com>
2021-08-04 12:48:34 -0300

    Add Texts to SwiperSlide

--- src/components/ContinentsSlide.tsx
+++ src/components/ContinentsSlide.tsx
@@ -3,7 +3,7 @@ import SwiperCore, { Pagination, Navigation } from "swiper/core";
 
 SwiperCore.use([Pagination, Navigation])
 
-import { Image } from "@chakra-ui/react"
+import { Flex, Heading, Text, Image } from "@chakra-ui/react"
 
 export function ContinentsSlide() {
   return (
@@ -15,6 +15,18 @@ export function ContinentsSlide() {
       className="mySwiper"
     >
       <SwiperSlide>
+        <Flex
+          w="100%"
+          h="100%"
+          pos="absolute"
+          zIndex="1"
+          flexDir="column"
+          justify="center"
+          align="center"
+        >
+          <Heading color="gray.100" fontSize="5xl">Europa</Heading>
+          <Text color="gray.200" fontSize="2xl">O continente mais antigo.</Text>
+        </Flex>
         <Image src={`https://images.unsplash.com/photo-1467269204594-9661b134dd2b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80`} />
       </SwiperSlide>
       <SwiperSlide>Slide 2</SwiperSlide>
