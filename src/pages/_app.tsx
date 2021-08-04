3676cf1bc15cccdc63ea325b7c5185aa336d5b72
Jhonata <gutemberg.jhonata@gmail.com>
2021-08-04 07:46:14 -0300

    Import Swiper Styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,12 @@ import { ChakraProvider } from "@chakra-ui/react"
 
 import { theme } from "../styles/theme"
 
+import "swiper/swiper.min.css";
+import "swiper/components/pagination/pagination.min.css"
+import "swiper/components/navigation/navigation.min.css"
+
+import "../styles/swiper.scss"
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
