f9d732bb269c2fa4406ba6052ea8b0142be50d02
Jhonata <gutemberg.jhonata@gmail.com>
2021-08-04 07:47:03 -0300

    Create ContinensSlide Component

---
+++ src/components/ContinentsSlide.tsx
@@ -0,0 +1,25 @@
+import { Swiper, SwiperSlide } from "swiper/react"
+import SwiperCore, { Pagination, Navigation } from "swiper/core";
+
+SwiperCore.use([Pagination, Navigation])
+
+import { Image } from "@chakra-ui/react"
+
+export function ContinentsSlide() {
+  return (
+    <Swiper
+      slidesPerView={1}
+      spaceBetween={30}
+      pagination={{ clickable: true }}
+      navigation={true}
+      className="mySwiper"
+    >
+      <SwiperSlide>
+        <Image src={`https://images.unsplash.com/photo-1467269204594-9661b134dd2b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80`} />
+      </SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+    </Swiper>
+  )
+}
