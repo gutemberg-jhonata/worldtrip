685f0ce42cce06a99919459b0d266ea9e2ebc491
Jhonata <gutemberg.jhonata@gmail.com>
2021-08-05 07:36:50 -0300

    Refactor Slide to get continents data as props

--- src/components/ContinentsSlide.tsx
+++ src/components/ContinentsSlide.tsx
@@ -5,7 +5,17 @@ SwiperCore.use([Pagination, Navigation])
 
 import { Flex, Heading, Text, Image } from "@chakra-ui/react"
 
-export function ContinentsSlide() {
+type Continent = {
+  name: string,
+  description: string,
+  image: string
+}
+
+type ContinentsSlideProps = {
+  continents: Continent[]
+}
+
+export function ContinentsSlide({ continents }: ContinentsSlideProps) {
   return (
     <Swiper
       slidesPerView={1}
@@ -14,24 +24,25 @@ export function ContinentsSlide() {
       navigation={true}
       className="mySwiper"
     >
-      <SwiperSlide>
-        <Flex
-          w="100%"
-          h="100%"
-          pos="absolute"
-          zIndex="1"
-          flexDir="column"
-          justify="center"
-          align="center"
-        >
-          <Heading color="gray.100" fontSize="5xl">Europa</Heading>
-          <Text color="gray.200" fontSize="2xl">O continente mais antigo.</Text>
-        </Flex>
-        <Image src={`https://images.unsplash.com/photo-1467269204594-9661b134dd2b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80`} />
-      </SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
+      {continents.map(continent => {
+        return (
+          <SwiperSlide>
+            <Flex
+              w="100%"
+              h="100%"
+              pos="absolute"
+              zIndex="1"
+              flexDir="column"
+              justify="center"
+              align="center"
+            >
+              <Heading color="gray.100" fontSize="5xl">{continent.name}</Heading>
+              <Text color="gray.200" fontSize="2xl">{continent.description}</Text>
+            </Flex>
+            <Image src={continent.image} />
+          </SwiperSlide>
+        )
+      })}
     </Swiper>
   )
 }
