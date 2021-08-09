40127a0c0c109ca2a594ecaccb970e392b9a439d
Jhonata <gutemberg.jhonata@gmail.com>
2021-08-09 07:36:38 -0300

    Create continent page

---
+++ src/pages/continent/[slug].tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import { Header } from "../../components/Header";
+
+export default function Continent() {
+  return (
+    <>
+      <Head>
+        <title>Continent | worldtrip</title>
+      </Head>
+
+      <Header />
+    </>
+  )
+}
