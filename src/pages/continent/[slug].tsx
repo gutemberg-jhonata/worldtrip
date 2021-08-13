141f8ddf8165b02514453d17fc23a3fe8de79ff7
Jhonata <gutemberg.jhonata@gmail.com>
2021-08-13 07:31:12 -0300

    Add Bio and ContinentInfo to Continent page

--- src/pages/continent/[slug].tsx
+++ src/pages/continent/[slug].tsx
@@ -1,6 +1,11 @@
-import Head from "next/head";
-import { ContinentBanner } from "../../components/ContinentBanner";
-import { Header } from "../../components/Header";
+import Head from "next/head"
+
+import { Box, Flex, HStack, Text } from "@chakra-ui/react"
+
+import { ContinentBanner } from "../../components/ContinentBanner"
+import { Header } from "../../components/Header"
+import { Container } from "../../components/Container"
+import { ContinentInfo } from "../../components/ContinentInfo"
 
 export default function Continent() {
   return (
@@ -12,6 +17,15 @@ export default function Continent() {
       <Header />
 
       <ContinentBanner />
+
+      <Container>
+        <HStack spacing="16" mt="20">
+          <Text fontSize="2xl">
+            A Europa é, por convenção, um dos seis continentes do mundo. Compreendendo a península ocidental da Eurásia, a Europa geralmente divide-se da Ásia a leste pela divisória de águas dos montes Urais, o rio Ural, o mar Cáspio, o Cáucaso, e o mar Negro a sudeste.
+          </Text>
+          <ContinentInfo />
+        </HStack>
+      </Container>
     </>
   )
 }
