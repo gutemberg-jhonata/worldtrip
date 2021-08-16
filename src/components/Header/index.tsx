33f98bfbbbba47ebbe205e42575fc4da2b41f077
Jhonata <gutemberg.jhonata@gmail.com>
2021-08-16 07:29:59 -0300

    Add navigation back in continent page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,43 @@
 import Link from "next/link"
 
-import { Flex, Image, Link as ChakraLink } from "@chakra-ui/react"
+import { Box, Image, Link as ChakraLink } from "@chakra-ui/react"
+import { ChevronLeftIcon } from "@chakra-ui/icons"
+
+import { Wrapper } from "../Wrapper"
+import { useRouter } from "next/dist/client/router"
+import { useEffect, useState } from "react"
 
 export function Header() {
+  const [isContinentsPage, setIsContinentsPage] = useState(false)
+
+  const router = useRouter()
+  const path = router.asPath
+
+  useEffect(() => {
+    setIsContinentsPage(path.includes('continent'))
+  }, [path])
+
   return (
-    <Flex align="center" justify="center" pt="6" pb="6" bg="">
-      <Link href="/">
-        <ChakraLink>
-          <Image src="/images/logo.svg" alt="worldtrip" />
-        </ChakraLink>
-      </Link>
-    </Flex>
+    <Box bgColor="white" py="6">
+      <Wrapper
+        display="flex"
+        flexDir="row"
+        justifyContent="center"
+        alignItems="center"
+        position="relative"
+      >
+        {isContinentsPage && (
+          <Box position="absolute" left="0">
+            <Link href="/">
+              <ChakraLink>
+                <ChevronLeftIcon w="32px" h="32px" color="gray.600" />
+              </ChakraLink>
+            </Link>
+          </Box>
+        )}
+
+        <Image src="/images/logo.svg" alt="worldtrip" />
+      </Wrapper>
+    </Box>
   )
 }
