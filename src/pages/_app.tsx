31e4a2661cf05efce8c3827f887786ebcf423f3b
Jhonata <gutemberg.jhonata@gmail.com>
2021-08-23 08:31:58 -0300

    Configure nprogress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,32 @@ import { AppProps } from "next/app"
 
 import { ChakraProvider } from "@chakra-ui/react"
 
-import { theme } from "../styles/theme"
+import NProgress from "nprogress"
+import { Router } from "next/dist/client/router"
 
-import "swiper/swiper.min.css";
+import { theme } from "../styles/theme"
+import "nprogress/nprogress.css"
+import "../styles/nprogress.scss"
+import "swiper/swiper.min.css"
 import "swiper/components/pagination/pagination.min.css"
 import "swiper/components/navigation/navigation.min.css"
-
 import "../styles/swiper.scss"
+
+NProgress.configure({
+  showSpinner: false
+})
+
+Router.events.on("routeChangeStart", () => {
+  NProgress.start();
+})
+
+Router.events.on("routeChangeComplete", () => {
+  NProgress.done();
+})
+
+Router.events.on("routeChangeError", () => {
+  NProgress.done();
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
