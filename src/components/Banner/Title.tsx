9b967b79d31df3cbc742b5fa464f1579d2ad07e0
Jhonata <gutemberg.jhonata@gmail.com>
2021-08-29 12:04:23 -0300

    Create Title Component

---
+++ src/components/Banner/Title.tsx
@@ -0,0 +1,14 @@
+import { Heading, Text } from "@chakra-ui/react";
+
+export function Title() {
+  return (
+    <Heading
+      color="gray.100"
+      fontWeight="medium"
+      fontSize={36}
+    >
+      6 Continentes,
+      <Text>infinitas possibilidades.</Text>
+    </Heading>
+  )
+}
