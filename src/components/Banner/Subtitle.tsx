045e4a5bb64c92eae2eec7a1357a9253fadddbd4
Jhonata <gutemberg.jhonata@gmail.com>
2021-08-29 12:04:11 -0300

    Create Subtitle Component

---
+++ src/components/Banner/Subtitle.tsx
@@ -0,0 +1,13 @@
+import { Text } from "@chakra-ui/react";
+
+export function Subtitle() {
+  return (
+    <Text
+      color="gray.200"
+      fontSize={20}
+      mt={4}
+    >
+      Chegou a hora de tirar do papel a viagem que você sempre sonhou.
+    </Text>
+  )
+}
